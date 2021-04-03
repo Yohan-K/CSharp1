8ca39f6c39e34dad5bbf0fde254428490082fc77
김정현 <rlawjdgus843@gmail.com>
2021-04-04 00:53:36 +0900

    Initial commit

---
+++ CSharp1/Player.cs
@@ -0,0 +1,76 @@
+﻿namespace CSharp1
+{
+    public enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+
+    class Player : Creature
+    {
+        protected PlayerType type = PlayerType.None;
+
+        protected Player(PlayerType type) : base(CreatureType.Player)
+        {
+            this.type = type;
+        }
+
+        public PlayerType GetPlayerType()
+        {
+            return type;
+            
+        }
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public int GetHp()
+        {
+            return hp;
+        }
+
+        public int GetAttack()
+        {
+            return attack;
+        }
+
+        public bool isDead()
+        {
+            return hp <= 0;
+        }
+
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0) hp = 0;
+        }
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
+    }
+}
